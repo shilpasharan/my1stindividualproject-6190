b53d3a2b5c46835671da9ba596c92b8dfe42d5c7
Shilpa Patil <shilpasharan@gmail.com>
2020-02-02 22:51:33 -0500

    adding something

--- main.py
+++ main.py
@@ -53,6 +53,15 @@ def pandas_sugar():
     df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
     return jsonify(df.to_dict())
 
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+    text = wikipedia.summary(company, sentences=10)
+    client = language.LanguageServiceClient()
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+    entities = client.analyze_entities(document).entities
+    return str(entities)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
