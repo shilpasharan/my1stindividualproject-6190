3626803289cbe0c152829e02826a31fb125da9ea
Shilpa Patil <shilpasharan@gmail.com>
2020-02-02 22:57:04 -0500

    adding something

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def wikipedia_route(company):
     entities = client.analyze_entities(document).entities
     return str(entities)
 
+    
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
